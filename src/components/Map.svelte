17e9ac752cf048fc97f0e9257eaaf1648eaa9a85
Stephanie Dang <stephanietumidang@gmail.com>
2024-10-25 00:57:00 -0700

    Cleaned out commented out code and backlog comments. Updated map attribute links.

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -23,12 +23,14 @@
         zoomControl: false
     }).setView([40.7128, -74.0060], 12);
 
+    map.attributionControl.setPrefix('')
+
     L.control.zoom({
         position: 'bottomright'
     }).addTo(map);
 
     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="https://leafletjs.com/" target="_blank" rel="noopener noreferrer">Leaflet</a> &copy; <a href="https://www.stadiamaps.com/" target="_blank" rel="noopener noreferrer">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank" rel="noopener noreferrer">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
   });
 </script>
